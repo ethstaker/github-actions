d73c8261b1b3bcba727c023098f6168cccb0c46d
hanniabu <hanniabu@gmail.com>
2024-10-17 11:13:07 -0400

    create blog feed proxy

--- _scripts/collect_data.py
+++ _scripts/collect_data.py
@@ -1,6 +1,7 @@
 import utilities
 from smoothing_pools import update_smoothing_pool_data
 from hardware import check_hardware_availability
+from blog_feed import update_blog_feed
 from job_listings import update_job_listings
 from for_hire_listings import update_for_hire_listings
 
@@ -9,6 +10,7 @@ from for_hire_listings import update_for_hire_listings
 def run_app():
   update_smoothing_pool_data()
   check_hardware_availability()
+  update_blog_feed()
   update_job_listings()
   update_for_hire_listings()
 
