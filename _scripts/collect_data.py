56a1f6e4da791413234b3e8957a0cae562471ad6
hanniabu <10511045+hanniabu@users.noreply.github.com>
2026-08-05 11:01:04 -0400

    Update collect_data.py

--- _scripts/collect_data.py
+++ _scripts/collect_data.py
@@ -8,10 +8,10 @@ from for_hire_listings import update_for_hire_listings
 
 
 def run_app():
-  update_smoothing_pool_data()
+  # update_smoothing_pool_data()
   check_hardware_availability()
   update_blog_feed()
-  update_job_listings()
+  # update_job_listings()
   # update_for_hire_listings()
 
 
